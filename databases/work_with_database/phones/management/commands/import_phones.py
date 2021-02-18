3b232d3c8b38c7862bd2a34599a07263218af504
Pfliger <pfliger@list.ru>
2021-02-18 16:08:18 +0300

    Completed "work_with_database"

--- databases/work_with_database/phones/management/commands/import_phones.py
+++ databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,5 +1,5 @@
 import csv
-
+from django.utils.text import slugify
 from django.core.management.base import BaseCommand
 from phones.models import Phone
 
@@ -14,7 +14,14 @@ class Command(BaseCommand):
             phone_reader = csv.reader(csvfile, delimiter=';')
             # пропускаем заголовок
             next(phone_reader)
-
             for line in phone_reader:
-                # TODO: Добавьте сохранение модели
-                pass
+                phone = Phone(
+                    id=int(line[0]),
+                    name=line[1],
+                    image=line[2],
+                    price=int(line[3]),
+                    release_date=line[4],
+                    lte_exists=line[5],
+                    slug=slugify(line[1]),
+                )
+                phone.save()
